e1fc8f79e743c2d12613ed58e1e8e4f96b9b6eb0
Yash Patel <officialy.yash@gmail.com>
2023-03-22 18:50:44 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
 
 
 // ROUTES
@@ -17,6 +19,11 @@ app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
 })
   
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
