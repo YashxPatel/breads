45a8042990783a94ce5b80b7ad9bea0a89d328bf
Yash Patel <officialy.yash@gmail.com>
2023-04-01 11:02:38 -0400

    Part 2 (Delete and Update)

--- views/index.jsx
+++ views/index.jsx
@@ -4,14 +4,13 @@ const Default = require('./layouts/Default')
 function Index ({breads, title})  {
     return (
       <Default title={title}>
-        {/*<h2>Index Page</h2>*/}
-        {/*<p>I have {breads[0].name} bread!</p>*/}
+        <h2>Index Page</h2>
         <ul>
             {
-                breads.map((bread, index) => {
+                breads.map((bread) => {
                     return (
-                        <li key={index}>
-                            <a href={`/breads/${index}`}>
+                        <li key={bread.id}>
+                            <a href={`/breads/${bread.id}`}>
                                 {bread.name}
                             </a>
                         </li>
